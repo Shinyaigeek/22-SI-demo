2779395c4ac499fff3579fa9b2f00df2824777b7
svpcadmin <PhilisPaxil@gmail.com>
2020-07-18 05:59:11 +0900

    fix:

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,77 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HTMLWebpackMultiBuildPlugin = require("html-webpack-multi-build-plugin");
 
-module.exports = {
-  mode: "production", 
-  entry: {
-    main: "./src/app.tsx",
-  },
-  output: {
-    path: path.join(__dirname, "public"),
-    publicPath: "/",
-    filename: "[name].bundle.js",
-    chunkFilename: "[name].bundle.js"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json", ".png", ".jpg"],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 3030,
-    hot: true,
-    historyApiFallback:true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(jpeg|png|jpg|gif)$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.(md)$/,
-        loader: "raw-loader"
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "src/index.html",
+require("dotenv").config();
+
+const template =
+  process.env.LEGACY === "true"
+    ? require.resolve("html-webpack-multi-build-plugin/template.ejs")
+    : require.resolve("html-webpack-plugin/default_index.ejs");
+
+module.exports = function (env) {
+  return {
+    mode: "production",
+    entry: {
+      main: "./src/app.tsx",
+    },
+    output: {
+      path: path.join(__dirname, "public"),
       publicPath: "/",
-    }),
-  ],
+      filename:
+        process.env.LEGACY === "true"
+          ? "[name].legacy.bundle.js"
+          : "[name].modern.bundle.js",
+      chunkFilename:
+        process.env.LEGACY === "true"
+          ? "[name].legacy.bundle.js"
+          : "[name].modern.bundle.js",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json", ".png", ".jpg"],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      port: 3030,
+      hot: true,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                compilerOptions: {
+                  target: process.env.LEGACY === "true" ? "ES5" : "ES2015",
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(jpeg|png|jpg|gif)$/,
+          loader: "file-loader",
+        },
+        {
+          test: /\.(md)$/,
+          loader: "raw-loader",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "src/index.html",
+        publicPath: "/",
+      }),
+      new HTMLWebpackMultiBuildPlugin()
+    ],
+  };
 };
