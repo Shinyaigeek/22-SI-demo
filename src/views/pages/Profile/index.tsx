df8e532e59308fa916e0e87092034be356c4e09f
svpcadmin <PhilisPaxil@gmail.com>
2020-07-18 02:26:47 +0900

    aadd: gallery

--- src/views/pages/Profile/index.tsx
+++ src/views/pages/Profile/index.tsx
@@ -3,10 +3,12 @@ import * as React from "react";
 import Icon from "../../../img/icon.png"
 
 import "./profile.scss"
+import { Gallery } from "../../components/gallery";
 
 export const Profile: React.FC<{}> = () => {
     return <div className="profile">
         <div className="name">Shinyaigeek</div>
         <img src={Icon} className="icon" />
+        <Gallery />
     </div>
 }
