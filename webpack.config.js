ddaac0eaf57e01bafe83c89cf9dd1defdf03d4c7
svpcadmin <PhilisPaxil@gmail.com>
2020-07-17 23:19:49 +0900

    fix: webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "none", 
+  mode: "production", 
   entry: {
     main: "./src/app.tsx",
   },
