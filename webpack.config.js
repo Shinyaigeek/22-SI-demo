57bdb8b03496064541cf8d10eb357a93a9f9a6e9
svpcadmin <PhilisPaxil@gmail.com>
2020-07-17 15:45:22 +0900

    set up: webpack, react, typescript

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/app.tsx",
+  },
+  output: {
+    path: path.join(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      // {
+      //   test: /\.(jpeg|png|jpg|gif)$/,
+      //   loader: "file-loader"
+      // }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ["main"],
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+};
